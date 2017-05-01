f0f02caf24f0d02462b8d74e206b977a9d747066
berkin <berkin.cirak@comeon.com>
2017-05-01 20:55:40 +0300

    configure store

---
+++ src/client/configureStore.js
@@ -0,0 +1,41 @@
+import { createStore } from 'redux'
+import appReducers from './reducers'
+
+const configureStore = () => {
+	const persistedState = {
+		jobs: {
+			allIds: ['random'],
+			byId: {
+				random: {
+					id: 'random',
+					title: 'random',
+					active: true,
+				}
+			}
+		}
+	}
+
+	const addLoggingToDispatch = (store) => {
+		const rawDispatch = store.dispatch
+		return (action) => {
+			console.group(action.type)
+			console.log('prev state', store.getState())
+			console.log('action', action)
+			const returnValue = rawDispatch(action)
+			console.log('next state', store.getState())
+			console.groupEnd(action.type)
+			return returnValue
+		}
+	}
+
+	const store = createStore(
+		appReducers,
+		persistedState
+	)
+
+	if (process.env.NODE_ENV !== 'production') {
+		store.dispatch = addLoggingToDispatch(store)
+	}
+	return store
+}
+export default configureStore
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,47 +1,15 @@
 import 'babel-polyfill'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import { AppContainer } from 'react-hot-loader'
 import App from './components/App'
-import appReducers from './reducers'
-
+import configureStore from './configureStore'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
-const persistedState = {
-	jobs: {
-		allIds: ['random'],
-		byId: {
-			random: {
-				id: 'random',
-				title: 'random',
-				active: true,
-			}
-		}
-	}
-}
-
-const addLoggingToDispatch = (store) => {
-	const rawDispatch = store.dispatch
-	return (action) => {
-		console.group(action.type)
-		console.log('prev state', store.getState())
-		console.log('action', action)
-		const returnValue = rawDispatch(action)
-		console.log('next state', store.getState())
-		console.groupEnd(action.type)
-		return returnValue
-	}
-}
-
-const store = createStore(appReducers, persistedState)
-if (process.env.NODE_ENV !== 'production') {
-	store.dispatch = addLoggingToDispatch(store)
-}
-
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+const store = configureStore()
 
 const wrapApp = AppComponent =>
 	<AppContainer>
@@ -50,12 +18,12 @@ const wrapApp = AppComponent =>
 		</Provider>
 	</AppContainer>
 
-ReactDOM.render(wrapApp(App), rootEl)
+render(wrapApp(App), rootEl)
 
 if (module.hot) {
 	module.hot.accept('./components/App', () => {
 		// eslint-disable-next-line global-require
 		const NextApp = require('./components/App').default
-		ReactDOM.render(wrapApp(NextApp), rootEl)
+		render(wrapApp(NextApp), rootEl)
 	})
 }
